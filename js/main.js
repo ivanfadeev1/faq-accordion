339db162a73cd8e676c6f63ec9c1f11a3bbef6ea
ivanfadeev1 <65688466+ivanfadeev1@users.noreply.github.com>
2023-12-26 06:21:33 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 const accordionHeaders = document.querySelectorAll('.accordion__header');
 
-accordionHeaders.forEach(accordionHeader => {
+accordionHeaders.forEach((accordionHeader, index) => {
     let target = accordionHeader.parentElement.nextElementSibling;
+
+    if (index === 0) {
+        accordionHeader.setAttribute('aria-expanded', 'true');
+        target.setAttribute('aria-hidden', 'false');
+        target.inert = false;
+        target.style.maxHeight = target.scrollHeight + 'px';
+        target.style.opacity = 1;
+        if (window.matchMedia('(max-width: 576px)').matches) {
+            target.style.marginTop = '4px';
+            target.style.marginBottom = '20px';
+        } else {
+            target.style.marginBottom = '24px';
+        }
+    }
 
     accordionHeader.onclick = () => {
         let expanded = accordionHeader.getAttribute('aria-expanded') === 'true';
